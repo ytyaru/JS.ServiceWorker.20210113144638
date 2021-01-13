22f68b395ece942e64262f62a5a84947328ea410
ytyaru <yttry0@gmail.com>
2021-01-13 17:01:08 +0900

    remove

--- docs/6/main.js
+++
@@ -1,7 +0,0 @@
-import ServiceWorkerTest from './ServiceWorkerTest.js';
-window.addEventListener('load', (event) => {
-//    console.log('has navigator:',navigator);
-//    console.log(`has serviceWorker: ${'serviceWorker' in navigator}`);
-//    let test = new ServiceWorkerTest(navigator.serviceWorker);
-    let test = new ServiceWorkerTest();
-});
--- docs/6/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python3 -m http.server 8000 &
-sleep 1
-chromium-browser http://0.0.0.0:8000/
-
